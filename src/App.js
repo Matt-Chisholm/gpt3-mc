4ed814a6ac283bb332f1f104da70cd51ee1cdf5c
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-06-21 09:05:12 -0700

    global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
 import "./App.css";
+import {
+  Footer,
+  Blog,
+  Header,
+  Features,
+  Possibility,
+  GPT3,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>GPT-3</header>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <GPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
