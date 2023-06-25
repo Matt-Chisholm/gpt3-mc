c3944dc4a354ac5c6ed529b8353d927cb55dc5bd
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-06-25 11:39:12 -0700

    header elements

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,6 +1,34 @@
 import React from "react";
 import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
-export default function Header() {
-  return <div>Header</div>;
-}
+const Header = () => {
+  return (
+    <div className='gpt3__header section__padding'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>
+          Let&apos;s Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          GPT-3 is the most powerful and advanced language model ever created.
+          It can write essays, summarize emails, and answer trivia questions. It
+          can even write code.
+        </p>
+        <div className='gpt3__header-content__input'>
+          <input type='email' placeholder='Your email address' />
+          <button type='button'>Get Started</button>
+        </div>
+        <div className='gpt3__header-content__people'>
+          <img src={people} alt='people' />
+          <p>1,600 people requested access in the last 24 hours</p>
+        </div>
+        <div className='gpt3__header-image'>
+          <img src={ai} alt='ai' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
