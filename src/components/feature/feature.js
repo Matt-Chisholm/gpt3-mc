3495895db83efba4ce545016e95e7a04ee99b78b
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-07-03 16:55:26 -0700

    fix

--- src/components/feature/feature.js
+++ src/components/feature/feature.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./feature.css";
 
-console.log("test");
-
 const Feature = ({ title, text }) => (
   <div className='gpt3__features-container__feature'>
     <div className='gpt3__features-container__feature-title'>
