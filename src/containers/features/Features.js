a78eb9b6e236fbd09f78e380c57ac22992fb751e
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-09-25 11:35:08 -0700

    features jsx

--- src/containers/features/Features.js
+++ src/containers/features/Features.js
@@ -1,6 +1,45 @@
 import React from "react";
 import "./features.css";
+import { Feature } from "../../components";
+
+const featuresData = [
+  {
+    title: "Improving end results instantly",
+    text: "GPT-3 is the most powerful and advanced language model ever created. It can write essays, summarize emails, and answer trivia questions. It can even write code.",
+  },
+  {
+    title: "Become more efficient",
+    text: "GPT-3 is the most powerful and advanced language model ever created. It can write essays, summarize emails, and answer trivia questions. It can even write code.",
+  },
+  {
+    title: "Save time and money",
+    text: "GPT-3 is the most powerful and advanced language model ever created. It can write essays, summarize emails, and answer trivia questions. It can even write code.",
+  },
+  {
+    title: "Get better results",
+    text: "GPT-3 is the most powerful and advanced language model ever created. It can write essays, summarize emails, and answer trivia questions. It can even write code.",
+  },
+];
 
 export default function Features() {
-  return <div>Features</div>;
+  return (
+    <div className='gpt3__features section__padding' id='features'>
+      <div className='gpt3__features-heading'>
+        <h1 className='gradient__text'>
+          The Future is Now and You Just Need to Get Started. Step Into the
+          Future Today & Make It Happen.
+        </h1>
+        <p>Request Early Access to Get Started.</p>
+      </div>
+      <div className='gpt3__features-container'>
+        {featuresData.map((feature, index) => (
+          <Feature
+            key={feature.title + index}
+            title={feature.title}
+            text={feature.text}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
